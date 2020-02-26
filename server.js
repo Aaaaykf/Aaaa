02a84dc43526ddeae81edf275e395c3da09bd82d
Glitch (hello-express) <none>
2020-02-26 17:06:32 +0000

    👂🚍 Checkpoint ./config.js:8374370/118 ./server.js:8374370/9685

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get("/", (request, response) => {
 });
 app.listen(process.env.PORT);
 setInterval(() => {
-  http.get(`http://brodcast6666.glitch.me/`);
+  http.get(`http://brodcast-tn.glitch.me/`);
 }, 280000);
 
 // كل البكجات الي ممكن تحتجها في اي بوت 
@@ -48,28 +48,145 @@ client.on('ready', () => {
 
  //كود برودكاست
 
-client.on("message", message => {
-
-            if (message.content.startsWith(prefix + "bc")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
- m.send(`${argresult}\n ${m}`);
-})
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
- message.delete(); 
-};     
+client.on("message", async message => {
+  if (!message.guild || message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+  if (message.content.startsWith(prefix + "bc")) {
+    if (!message.member.hasPermission("ADMINISTRATOR")) return;
+    let args = message.content
+      .split(" ")
+      .slice(1)
+      .join(" ");
+    message.channel
+      .send(
+        ">>> **[1] جميع الاعضاء\n[2] الاعضاء المتصلين\n[3] الرتب الخاصة\n[0] الغاء الأمر**"
+      )
+      .then(m => {
+        message.channel
+          .awaitMessages(msg => msg.author.id === message.author.id, {
+            max: 1,
+            time: 1000 * 60 * 2,
+            errors: ["time"]
+          })
+          .then(c => {
+            if (c.first().content === "1") {
+              message.guild.members.forEach(m => {
+                m.send(`${args}\n`).catch(err => {
+                  if (err) throw err;
+                });
+              });
+              c.first().delete();
+              m.delete();
+              message.channel.send("**تم نشر الرسالة بنجاح**");
+            }
+            if (c.first().content === "2") {
+              message.guild.members
+                .filter(m => m.presence.status !== "offline")
+                .forEach(m => {
+                  m.send(`${args}\n`).catch(err => {
+                    if (err) throw err;
+                  });
+                });
+              c.first().delete();
+              m.delete();
+              message.channel.send("**تم نشر الرسالة بنجاح**");
+            }
+            if (c.first().content == "0") {
+              c.first().delete();
+              m.delete();
+              message.channel.send("**تم الغاء الامر بنجاح**");
+            }
+            if (c.first().content === "3") {
+              m.edit("**>>> ادخل اسم الرتبة من فضلك**").then(ms => {
+                message.channel
+                  .awaitMessages(msg => msg.author.id === message.author.id, {
+                    max: 1,
+                    time: 1000 * 60 * 2,
+                    errors: ["time"]
+                  })
+                  .then(c => {
+                    let role = message.guild.roles.find(
+                      role => role.name === c.first().content
+                    );
+                    if (!role)
+                      return message.channel
+                        .send("**:x: لا استطيع العثور على الرتبة الخاصة بالرسالة**")
+                        .then(() => {
+                          ms.delete();
+                          c.first().delete();
+                        });
+                    let roleID = role.id;
+                    message.guild.roles.get(roleID).members.forEach(m => {
+                      m.send(`${args}\n`).catch(err => {
+                        if (err) throw err;
+                      });
+                    });
+                    c.first().delete();
+                    m.delete();
+              message.channel.send("**تم نشر الرسالة بنجاح**");
+                  });
+              });
+            }
+          })
+          .catch(() => m.delete());
+      });
+  } else if (message.content.startsWith(prefix + "setname")) {
+    let args = message.content
+      .split(" ")
+      .slice(1)
+      .join(" ");
+    if (!message.author.id === "335645388323160064") return;
+    client.user.setUsername(args);
+    message.channel.send(`تم تغيير الاسم الى ..**${args}** `);
+  } else if (message.content.startsWith(prefix + "setavatar")) {
+    let args = message.content
+      .split(" ")
+      .slice(1)
+      .join(" ");
+    if (!message.author.id === "335645388323160064") return;
+    client.user.setAvatar(args).catch(err => message.reply("send a valid url"));
+    message.channel.send(`تم تغيير الصورة الى :**${args}** `);
+  }
 });
 
+///تغير الحالة
 
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : osama tech`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : osama tech ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`12K Subscribers `,"http://twitch.tv/S-F")
-client.user.setStatus("dnd")
+client.on('ready',  () => {
+  console.log("ffff");
 });
+
+const developers = ['335645388323160064'] //الايدي هنا
+const adminprefix = "#"//برفكس
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'pl')) {
+    client.user.setGame(argresult);
+      message.channel.send("**:white_check_mark: | The Playing Status Has Been Changed To : ``"
+   + `${argresult}` + "``**")
+  } else 
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send("**:white_check_mark: | The Watching Status Has Been Changed To : ``"
+   + `${argresult}` + "``**")
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send("**:white_check_mark: | The Listening Status Has Been Changed To : ``"
+   + `${argresult}` + "``**")
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/Rabea-YassiN");
+      message.channel.send("**:white_check_mark: | The Streaming Status Has Been Changed To : ``"
+   + `${argresult}` + "``**")
+
+}
+});
+
+//اختصارات للاوامر
+//بلاينق ، $pl هنا اللي تبي ينكتب
+//ستريمنق ، $st هنا اللي تبي ينكتب
+//لسنق ، $ls هنا اللي تبي ينكتب
+//ويتشنق ، $wt هنا اللي تبي ينكتب
+
