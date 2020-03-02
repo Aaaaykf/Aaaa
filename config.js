fb38163515af4e495d7d826d801ce4684292ca52
Glitch (hello-express) <none>
2020-03-02 15:30:31 +0000

    👵🕴 Checkpoint ./config.js:8374370/118

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    TOKEN: 'NjYxNjYwNjg4NTYwODE2MTM5.XlGpcA.hD1tVf2jRuHvSc0QRM-a8jguucA',    
+    TOKEN: 'NjYxNjYwNjg4NTYwODE2MTM5.Xl0mXg.38QJmQLxF2_Ibbilu6hs_HcicCQ',    
     YT_API_KEY: 'api ', 
     prefix: '$',
     devs: ['359541019836022784']
