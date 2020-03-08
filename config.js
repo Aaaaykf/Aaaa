e0e60a0af886ffcb3fce7576a696c96d2f2d810e
Glitch (hello-express) <none>
2020-03-08 14:20:27 +0000

    🙇🏛 Checkpoint ./config.js:8374370/118

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    TOKEN: 'Njg1OTU0NTE3NzY5OTEyMzYw.XmQLDw.nAHA3y008io4QSahRHO4JFTVAFY',    
+    TOKEN: 'Njg2MjE0NjgzNzc4MDg5MDU1.XmT9VQ.g1B-l2WG_fqcaizPLL4ETT1bcZw',    
     prefix: '#',
     devs: ['608382291697467412']
 }
