f733c56ad2750fd2bfa7acf3268e3e638a974743
Glitch (hello-express) <none>
2020-04-17 19:53:45 +0000

    🎗💶 Checkpoint ./config.js:15014198/118

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    TOKEN: 'Njk2NzM5NjAxNDI3MDcxMDM4.XpbNew.WHtX8ICPZ6LSSdynM0VoXCVIp-o',    
+    TOKEN: 'NDY1NTU1MTg4MDc4NjA4NDA0.XpoIRw.1_JAFklXiL51nvbx80sf1b_WYFY',    
     prefix: '#',
     devs: ['416602464020594698']
 }
