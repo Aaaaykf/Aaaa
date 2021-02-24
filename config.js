4ce6c2fc75cb4b7415a635eedbd3bcb943c22e9d
Aaaaykf <77963750+Aaaaykf@users.noreply.github.com>
2021-02-24 21:12:38 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 exports.Token = "ODExMzk5MzU2OTE3MzUwNDEw.YCxoiQ.57y9U3JO1PqRa9bVfHzhAtpLlW4"; //Bot Token - Important
 exports.Owner = ""; //Bot Owner ID - Important
-exports.Default_Prefix = ""; //Bot Default Prefix (Examples: A!, !)- Important
+exports.Default_Prefix = "/4"; //Bot Default Prefix (Examples: A!, !)- Important
 exports.Color = "RANDOM"; //Bot All Embeds Color - Use CAPS For Name (Examples: BLUE, RANDOM) - Important
